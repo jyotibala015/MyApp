2279ab7675953c0ab4e7e7937d5c699adf2c527a
jyotibala015 <jyotibala015@gmail.com>
2021-03-11 23:02:21 +0530

    First Commit

---
+++ MessageRunner.java
@@ -0,0 +1,15 @@
+package com.jyotisoft.runner;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class MessageRunner implements CommandLineRunner {
+
+	@Override
+	public void run(String... args) throws Exception {
+		int i = 10;
+		System.out.println("HELLO.."+i);
+	}
+
+}
